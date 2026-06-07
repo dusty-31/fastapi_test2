b04c2198d073ab685d9ae3e453704ea867bee26c
Yuriy Rybak <jura.ryback007@gmail.com>
2026-06-07 23:47:36 +0300

    Added Docker setups for backend and db.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+
+from settings.db import ping
 
 app = FastAPI()
 
@@ -6,6 +8,17 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World!"}
+
+
+@app.get("/health_check", status_code=status.HTTP_200_OK)
+async def db_healthcheck():
+    is_alive = await ping()
+    if not is_alive:
+        raise HTTPException(
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+            detail="Database connection failed",
+        )
+    return {"status": "healthy", "database": "connected"}
 
 
 if __name__ == "__main__":
